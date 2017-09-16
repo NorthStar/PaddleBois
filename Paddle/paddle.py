023537e51ee6e5631a520f687000b5511956a03e
nicholasroberson <roberson@grinnell.edu>
2017-09-16 16:28:22 -0400

    added code to download necessary packages and files

--- Paddle/paddle.py
+++ Paddle/paddle.py
@@ -5,7 +5,7 @@ from scipy import spatial
 import numpy
 
 
-
+# 1
 class word2vec:
 
     word_dict = dict()
@@ -13,13 +13,18 @@ class word2vec:
 
     def __init__(self):
         if not os.path.isfile("/models/word2vec/inference_topology.pkl"):
-            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/04.word2vec/inference_topology.pkl", "models/word2vec/inference_topology.pkl")
+            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/04.word2vec/inference_topology.pkl",
+                               "models/word2vec/inference_topology.pkl")
         if not os.path.isfile("/models/word2vec/param.tar"):
-            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/04.word2vec/param.tar", "models/word2vec/param.tar")
+            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/04.word2vec/param.tar",
+                               "models/word2vec/param.tar")
         if not os.path.isfile("/models/word2vec/word_dict"):
-            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/04.word2vec/word_dict", "models/word2vec/word_dict")
+            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/04.word2vec/word_dict",
+                               "models/word2vec/word_dict")
         if not os.path.isfile("/models/word2vec/embedding_table"):
-            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/04.word2vec/embedding_table", "models/word2vec/embedding_table")
+            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/04.word2vec/embedding_table",
+                               "models/word2vec/embedding_table")
+
         self.word_dict = dict()
         with open("models/word2vec/word_dict", "r") as f:
             for line in f:
@@ -36,17 +41,62 @@ class word2vec:
         i1 = int(self.word_dict[s1])
         i2 = int(self.word_dict[s2])
         return spatial.distance.cosine(self.embedding_table[i1], self.embedding_table[i2])
+# 2
+class image_classification:
 
+    def __init__(self):
+        if not os.path.isfile("models/image_classification/inference_topology.pkl"):
+            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/03.image_classification/inference_topology.pkl",
+                               "models/image_classification/inference_topology.pkl")
 
+        if not os.path.isfile("models/image_classification/param.tar"):
+            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/03.image_classification/param.tar",
+                               "models/image_classification/param.tar")
+
+# 3
 class sentiment_classification:
-    if not os.path.isfile("inference_topology.pkl"):
-            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/04.word2vec/inference_topology.pkl", "inference_topology.pkl")
-        if not os.path.isfile("param.tar"):
-            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/04.word2vec/param.tar", "param.tar")
-        if not os.path.isfile("word_dict"):
-            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/04.word2vec/word_dict", "word_dict")
-     
 
-        
+    def __init__(self):
+        if not os.path.isfile("models/sentiment_classification/inference_topology.pkl"):
+            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/06.understand_sentiment/inference_topology.pkl",
+                               "models/sentiment_classification/inference_topology.pkl")
 
+        if not os.path.isfile("models/sentiment_classification/param.tar"):
+            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/06.understand_sentiment/param.tar",
+                               "models/sentiment_classification/param.tar")
 
+        if not os.path.isfile("models/sentiment_classification/word_dict.tar"):
+            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/06.understand_sentiment/word_dict.tar",
+                               "models/sentiment_classification/word_dict/tar")
+
+# 4
+class machine_translation:
+
+    def __init__(self):
+        if not os.path.isfile("models/machine_translation/inference_topology.pkl"):
+            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/08.machine_translation/inference_topology.pkl",
+                               "models/machine_translation/inference_topology.pkl")
+
+        if not os.path.isfile("models/machine_translation/param.tar"):
+            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/08.machine_translation/param.tar",
+                               "models/machine_translation/param.tar")
+
+        if not os.path.isfile("models/machine_translation/src_dict.txt"):
+            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/08.machine_translation/src_dict.txt",
+                               "models/machine_translation/src_dict.txt")
+
+        if not os.path.isfile("models/machine_translation/trg_dict.txt"):
+            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/08.machine_translation/trg_dict.txt",
+                               "models/machine_translation/trg_dict.txt")
+
+# 5
+class recognize_digits:
+
+    def __init__(self):
+        if not os.path.isfile("models/recognize_digits/inference_topology.pkl"):
+            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/inference_topology.pkl",
+                               "models/recognize_digits/inference_topology.pkl")
+
+        if not os.path.isfile("models/recognize_digits/param.tar"):
+            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/param.tar",
+                               "models/recognize_digits/param.tar")
