d65f1e92ad607309145a37cd0a7a40382bde5605
nicholasroberson <roberson@grinnell.edu>
2017-09-16 15:49:05 -0400

    added models/word2vec/ folder to import files

--- Paddle/paddle.py
+++ Paddle/paddle.py
@@ -12,20 +12,20 @@ class word2vec:
     embedding_table = ""
 
     def __init__(self):
-        if not os.path.isfile("inference_topology.pkl"):
-            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/04.word2vec/inference_topology.pkl", "inference_topology.pkl")
-        if not os.path.isfile("param.tar"):
-            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/04.word2vec/param.tar", "param.tar")
-        if not os.path.isfile("word_dict"):
-            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/04.word2vec/word_dict", "word_dict")
-        if not os.path.isfile("embedding_table"):
-            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/04.word2vec/embedding_table", "embedding_table")
+        if not os.path.isfile("/models/word2vec/inference_topology.pkl"):
+            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/04.word2vec/inference_topology.pkl", "models/word2vec/inference_topology.pkl")
+        if not os.path.isfile("/models/word2vec/param.tar"):
+            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/04.word2vec/param.tar", "models/word2vec/param.tar")
+        if not os.path.isfile("/models/word2vec/word_dict"):
+            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/04.word2vec/word_dict", "models/word2vec/word_dict")
+        if not os.path.isfile("/models/word2vec/embedding_table"):
+            urllib.urlretrieve("https://s3.us-east-2.amazonaws.com/models.paddlepaddle/04.word2vec/embedding_table", "models/word2vec/embedding_table")
         self.word_dict = dict()
-        with open("word_dict", "r") as f:
+        with open("models/word2vec/word_dict", "r") as f:
             for line in f:
                 key, value = line.strip().split(" ")
                 self.word_dict[key] = value
-        self.embedding_table = numpy.loadtxt("embedding_table", delimiter=",")
+        self.embedding_table = numpy.loadtxt("models/word2vec/embedding_table", delimiter=",")
 
     def run(self, s1, s2):
         if s1 in self.word_dict:
